19e1764575d5824d181af05c94b97885ebe5599c
Francesco Cusumano <cusumanof11@gmail.com>
2020-07-15 11:06:56 +0200

    Testing 1: Case 1 => False Case 4 => True Case 0 => Exception Case<0 => False Case%100 => False

---
+++ Bissextile/Bissextile/BissextileServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Bissextile
+{
+    public class BissextileServices
+    {
+        public static bool IsBissextile(int year)
+        {
+            if (year == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            if (year < 0)
+            {
+                return false;
+            }
+            if (year % 100 == 0)
+            {
+                return false;
+            }
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Bissextile/BissextileTest/BissextileTest.cs
@@ -0,0 +1,52 @@
+using Bissextile;
+using NUnit.Framework;
+using System;
+
+namespace BissextileTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(1)]
+        [TestCase(-20)]
+        [TestCase(1)]
+        [TestCase(1)]
+        [TestCase(1)]
+        [TestCase(1)]
+        [TestCase(1)]
+        public void ShouldReturn_False(int year)
+        {
+            Assert.That(BissextileServices.IsBissextile(year), Is.False);
+        }
+
+        [TestCase(4)]
+        [TestCase(4)]
+        [TestCase(4)]
+        [TestCase(4)]
+        [TestCase(4)]
+        [TestCase(4)]
+        [TestCase(4)]
+        public void ShouldReturn_True(int year)
+        {
+            Assert.That(BissextileServices.IsBissextile(year), Is.True);
+        }
+
+        [Test]
+        public void ShouldReturnException()
+        {
+            try
+            {
+                BissextileServices.IsBissextile(0);
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.Pass();
+            }
+            Assert.Fail();
+        }
+    }
+}
