2fbcd00421d7468263532edcfd3c9d73d99a06b9
Francesco Cusumano <cusumanof11@gmail.com>
2020-07-15 10:33:03 +0200

    FactorTesting Completo

--- PrimeFactor/PrimeFactor/PrimeFactors.Service.cs
+++ PrimeFactor/PrimeFactor/PrimeFactors.Service.cs
@@ -9,10 +9,40 @@ namespace PrimeFactorsService
         public static List<int> Calculate(int x)
         {
             List<int> factors = new List<int>();
-            if (x != 1)
+            if (x <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            int qq;
+            int tmpx = x;
+            if (tmpx % 2 != 0)
+            {
+                qq = (tmpx - 1) / 2;
+            }
+            else
+            {
+                qq = tmpx / 2;
+            }
+            int i = 2;
+            
+            while (i <= qq)
+            {
+                if (tmpx % i == 0)
+                {
+                    factors.Add(i);
+                    tmpx = tmpx / i;
+                    i = 2;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (factors.Count == 0 && x!=1)
             {
                 factors.Add(x);
             }
+            
             return factors;
         }
     }
--- PrimeFactor/PrimeFactorTest/PrimeFactorUnitTest.cs
+++ PrimeFactor/PrimeFactorTest/PrimeFactorUnitTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using NUnit.Framework;
 using PrimeFactorsService;
@@ -35,6 +36,90 @@ namespace PrimeFactorTest
             List<int> expectedValue = new List<int> { 3 };
 
             Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void FourShouldReturn_TwoTwo()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(4);
+            List<int> expectedValue = new List<int> { 2 , 2 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void FiveShouldReturn_Five()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(5);
+            List<int> expectedValue = new List<int> { 5 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void SixShouldReturn_TwoThree()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(6);
+            List<int> expectedValue = new List<int> { 2, 3 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        public void SevenShouldReturn_Seven()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(7);
+            List<int> expectedValue = new List<int> { 7 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+
+        [Test]
+        public void EightShouldReturn_TwoTwoTwo()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(8);
+            List<int> expectedValue = new List<int> { 2, 2, 2 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void NineShouldReturn_ThreeThree()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(9);
+            List<int> expectedValue = new List<int> { 3, 3 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void TenShouldReturn_TwoFive()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(10);
+            List<int> expectedValue = new List<int> { 2, 5};
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void ShouldReturn_Five_TwoHundredSixtyNine()
+        {
+            List<int> primeFactors = PrimeFactors.Calculate(1345);
+            List<int> expectedValue = new List<int> { 5, 269 };
+
+            Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        }
+        [Test]
+        public void ShouldReturnException()
+        {
+            try
+            {
+                List<int> primeFactors = PrimeFactors.Calculate(-1);
+            }
+            catch (InvalidOperationException)
+            {
+                Assert.Pass();
+            }
+            Assert.Fail();            
+        }
+        [Test]
+        public void Zero_ShouldReturnException()
+        {
+            Assert.That(()=>PrimeFactors.Calculate(0), Throws.InvalidOperationException);
         }
     }
 }
