983299e45512d1cd9b42ec42c084546bb6d289d7
Francesco Cusumano <cusumanof11@gmail.com>
2020-07-15 10:49:50 +0200

    Ristrutturazione Codice

--- PrimeFactor/PrimeFactorTest/PrimeFactorUnitTest.cs
+++ PrimeFactor/PrimeFactorTest/PrimeFactorUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NUnit.Framework;
 using PrimeFactorsService;
 
@@ -21,89 +22,111 @@ namespace PrimeFactorTest
             Assert.That(primeFactors, Is.Empty);
         }
 
-        [Test]
-        public void TwoShouldReturn_Two()
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 2, 2)]
+        [TestCase(5, 5)]
+        [TestCase(6, 2, 3)]
+        [TestCase(7, 7)]
+        [TestCase(8, 2, 2, 2)]
+        [TestCase(9, 3, 3)]
+        [TestCase(10, 2, 5)]
+        [TestCase(11, 11)]
+        [TestCase(1345, 5, 269)]
+        public void ShouldReturn(int x, params int[] expectedFactors)
         {
-            List<int> primeFactors = PrimeFactors.Calculate(2);
-            List<int> expectedValue = new List<int> { 2 };
+            List<int> primeFactors = PrimeFactors.Calculate(x);
+            List<int> expectedValue = expectedFactors.ToList();
 
             Assert.That(primeFactors, Is.EqualTo(expectedValue));
         }
 
-        [Test]
-        public void ThreeShouldReturn_Three()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(3);
-            List<int> expectedValue = new List<int> { 3 };
+        #region  Old Methods 
+        //[Test]
+        //public void TwoShouldReturn_Two()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(2);
+        //    List<int> expectedValue = new List<int> { 2 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
-        [Test]
-        public void FourShouldReturn_TwoTwo()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(4);
-            List<int> expectedValue = new List<int> { 2 , 2 };
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
-        [Test]
-        public void FiveShouldReturn_Five()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(5);
-            List<int> expectedValue = new List<int> { 5 };
+        //[Test]
+        //public void ThreeShouldReturn_Three()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(3);
+        //    List<int> expectedValue = new List<int> { 3 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
-        [Test]
-        public void SixShouldReturn_TwoThree()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(6);
-            List<int> expectedValue = new List<int> { 2, 3 };
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        //[Test]
+        //public void FourShouldReturn_TwoTwo()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(4);
+        //    List<int> expectedValue = new List<int> { 2 , 2 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        //[Test]
+        //public void FiveShouldReturn_Five()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(5);
+        //    List<int> expectedValue = new List<int> { 5 };
 
-        [Test]
-        public void SevenShouldReturn_Seven()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(7);
-            List<int> expectedValue = new List<int> { 7 };
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        //[Test]
+        //public void SixShouldReturn_TwoThree()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(6);
+        //    List<int> expectedValue = new List<int> { 2, 3 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
 
-        [Test]
-        public void EightShouldReturn_TwoTwoTwo()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(8);
-            List<int> expectedValue = new List<int> { 2, 2, 2 };
+        //[Test]
+        //public void SevenShouldReturn_Seven()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(7);
+        //    List<int> expectedValue = new List<int> { 7 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
-        [Test]
-        public void NineShouldReturn_ThreeThree()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(9);
-            List<int> expectedValue = new List<int> { 3, 3 };
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
-        [Test]
-        public void TenShouldReturn_TwoFive()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(10);
-            List<int> expectedValue = new List<int> { 2, 5};
+        //[Test]
+        //public void EightShouldReturn_TwoTwoTwo()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(8);
+        //    List<int> expectedValue = new List<int> { 2, 2, 2 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
-        [Test]
-        public void ShouldReturn_Five_TwoHundredSixtyNine()
-        {
-            List<int> primeFactors = PrimeFactors.Calculate(1345);
-            List<int> expectedValue = new List<int> { 5, 269 };
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        //[Test]
+        //public void NineShouldReturn_ThreeThree()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(9);
+        //    List<int> expectedValue = new List<int> { 3, 3 };
 
-            Assert.That(primeFactors, Is.EqualTo(expectedValue));
-        }
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        //[Test]
+        //public void TenShouldReturn_TwoFive()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(10);
+        //    List<int> expectedValue = new List<int> { 2, 5};
+
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        //[Test]
+        //public void ShouldReturn_Five_TwoHundredSixtyNine()
+        //{
+        //    List<int> primeFactors = PrimeFactors.Calculate(1345);
+        //    List<int> expectedValue = new List<int> { 5, 269 };
+
+        //    Assert.That(primeFactors, Is.EqualTo(expectedValue));
+        //}
+        #endregion
+
         [Test]
         public void ShouldReturnException()
         {
